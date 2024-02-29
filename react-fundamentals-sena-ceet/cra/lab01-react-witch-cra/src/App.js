8c157d4073c2c442947c9c44564ed91d683b50fd
judynataliacl <juncorrea@soy.sena.edu.co>
2024-02-28 23:34:15 -0500

    Agregado lab01-react-witch-cra

---
+++ react-fundamentals-sena-ceet/cra/lab01-react-witch-cra/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hola este es mi primer componente de React</h1>
+      <p>SENA-CEET</p>
+    </>
+  );
+}
+
+export default App;
