d6d0db63df066dfffa949068ec2e3c01eabcb7bf
judynataliacl <juncorrea@soy.sena.edu.co>
2024-02-29 01:24:51 -0500

    Crear lab 2,Principio de Responsabilidad Única

---
+++ 02-escribir-marcado-jsx/src/components/MyButton.jsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+function MyButton() {
+    return (
+      <a href="https://github.com/Manuelyjc/Laboratorios_React"><button className="button">Soy un botón</button></a>
+    );
+  }
+
+export default MyButton;
