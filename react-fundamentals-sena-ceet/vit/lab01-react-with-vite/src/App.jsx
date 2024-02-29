7890173435308a0eb8b82455d3fccc7fbf451dda
judynataliacl <juncorrea@soy.sena.edu.co>
2024-02-28 22:25:55 -0500

    (feat): add  diferencia de vit a cra

---
+++ react-fundamentals-sena-ceet/vit/lab01-react-with-vite/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1>Hola mundo es mi primer componente en vite</h1>
+        <p>SENA-CEET</p>
+    </>
+  )
+}
+
+export default App
