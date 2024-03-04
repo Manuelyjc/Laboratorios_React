d1028b22b919aacfcb8381632020ac02b56d8ec7
judynataliacl <juncorrea@soy.sena.edu.co>
2024-03-04 16:09:00 -0500

    (feat): add Crear lab 03.

---
+++ 03-agregando-estilos/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App(){
+    return(
+        <>
+            <img className="avatar"  src="src/assets/imagen/parliament.jpg" alt="Houses of Parliament by Claude Monet"/>
+        </>
+    )
+}
+export default App
+
