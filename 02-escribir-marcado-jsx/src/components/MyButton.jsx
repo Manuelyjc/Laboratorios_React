10714aab6dd19a4d139c84c35c64f2ef4b3ec3dd
judynataliacl <juncorrea@soy.sena.edu.co>
2024-03-04 16:21:10 -0500

    (feat): eliminar carpetas sobrantes.

--- 02-escribir-marcado-jsx/src/components/MyButton.jsx
+++
@@ -1,9 +0,0 @@
-import '../App.css'
-
-function MyButton() {
-    return (
-      <a href="https://github.com/Manuelyjc/Laboratorios_React"><button className="button">Soy un botón</button></a>
-    );
-  }
-
-export default MyButton;
