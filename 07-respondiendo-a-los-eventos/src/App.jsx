ddaf45188ce115d3dcd13ed12224ac68920fb699
Manuelyjc <114593318+Manuelyjc@users.noreply.github.com>
2024-03-04 20:00:46 -0500

    Update App.jsx

--- 07-respondiendo-a-los-eventos/src/App.jsx
+++ 07-respondiendo-a-los-eventos/src/App.jsx
@@ -1,11 +1,26 @@
-import ShoppingList from "./components/ShoppingList.jsx";
 import './App.css'
+import MyButton from "./components/MyButton.jsx";
 
-function App() {
+
+function MyMensaje() {
     return (
-        <section>
-            <ShoppingList/>
-        </section>
-    )
+      <div className="mensaje"> 
+      <p> función que se ejecuta en respuesta a un evento específico  de un componente que <br></br>
+          ocurre dentro de un programa o aplicación. Oprime el boton!!.  <br></br>
+          </p>
+         </div>
+    );
+  }
+
+function  App(){
+    return(
+        <>
+            <h1>Responder a eventos</h1>
+            <MyMensaje/>
+            <br></br>
+            <MyButton/>
+        </>
+    );
 }
-export default App
+
+export default App;
